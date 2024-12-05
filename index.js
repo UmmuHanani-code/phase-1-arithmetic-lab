2eece111b26e0953220c773fea7c1b4cdb649e98
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2024-12-05 16:16:23 +0300

    commit all

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random;
+random = Math.floor(Math.random())*10 + 1;
+ Number.isInteger(random);
+
+ let num3 = 14;
+ let num4 = 10;
+ let mod;
+
+ mod = num3 % num4;
+
+ console.log(mod);
+
+ const max = Math.max(5, 10, 15, 20);
+ console.log(max);
